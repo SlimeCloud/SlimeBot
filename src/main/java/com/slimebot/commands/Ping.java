49fdae6cc2d637d629b04c1cfa679928f9b46158
Matito <matitos@outlook.de>
2023-04-27 23:27:42 +0200

    Add Ping cmd
    Rewrite from py Bot..

---
+++ src/main/java/com/slimebot/commands/Ping.java
@@ -0,0 +1,31 @@
+package com.slimebot.commands;
+
+import dev.matito.Main;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class Ping extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        super.onSlashCommandInteraction(event);
+
+        if (!(event.getName().equals("ping"))){return;}
+
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("Pong")
+                .setDescription(event.getJDA().getGatewayPing() + "ms")
+                .setColor(Main.embedColor);
+
+
+        MessageEmbed em = embed.build();
+
+
+        event.replyEmbeds(em).setEphemeral(true).queue();
+
+    }
+}
