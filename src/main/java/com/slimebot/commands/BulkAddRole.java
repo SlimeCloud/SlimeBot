ff322a32809ca248fce4f9c2398ef1d3471e5036
Matito <matitos@outlook.de>
2023-04-27 23:27:27 +0200

    Add BulkAddRole Feature
    Rewrite from py Bot..

---
+++ src/main/java/com/slimebot/commands/BulkAddRole.java
@@ -0,0 +1,63 @@
+package com.slimebot.commands;
+
+
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class BulkAddRole extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        super.onSlashCommandInteraction(event);
+
+        if (!(event.getName().equals("role_check"))){return;}
+
+        Member cmdRunner = Objects.requireNonNull(event.getGuild()).getMemberById(event.getUser().getId());
+
+        Role staffRole = event.getGuild().getRoleById("1081650648124248124"); //ToDo get ID from a Config eg. Settings
+
+
+
+        assert cmdRunner != null;
+        if (!(cmdRunner.getRoles().contains(staffRole))) {
+            event.reply("kein Teammitglied!").queue();
+            return;
+        }
+
+        ArrayList<Member> memberWithout = new ArrayList<>();
+
+        OptionMapping botsOption = event.getOption("bots");
+        OptionMapping roleOption = event.getOption("rolle");
+
+
+
+        for (Member member: event.getGuild().getMembers()) {
+            assert botsOption != null;
+            User memberAsUser = (User) member;
+            if (!botsOption.getAsBoolean() && memberAsUser.isBot()){ //ToDo add check if member is a Bot
+                continue;
+            }
+
+            assert roleOption != null;
+            if (member.getRoles().contains(roleOption.getAsRole())){
+                continue;
+            } else {
+                event.getGuild().addRoleToMember(member, Objects.requireNonNull(event.getGuild().getRoleById(roleOption.getAsString()))).queue();
+                memberWithout.add(member);
+            }
+            String abc = "abc";
+
+        }
+
+        assert roleOption != null;
+        event.reply("Die Rolle " + roleOption.getAsRole().getAsMention() + " wurde " + (long) memberWithout.size() + " Member gegeben!").queue();
+
+
+    }
+}
