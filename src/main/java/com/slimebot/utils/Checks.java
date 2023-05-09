8fa36539a29b49c4a3da789718ba30939f8967a2
Matito <matitos@outlook.de>
2023-05-09 23:02:50 +0200

    remove unused println

--- src/main/java/com/slimebot/utils/Checks.java
+++ src/main/java/com/slimebot/utils/Checks.java
@@ -7,7 +7,6 @@ import net.dv8tion.jda.api.entities.Role;
 public class Checks {
 
     public static Boolean hasTeamRole(Member member, Guild guild){
-        System.out.println(member.getRoles() + guild.getId());
         Role staffRole = guild.getRoleById("1081650648124248124");  //ToDo get ID from a Config eg. Settings
                                                                     //MainID: 1077266943003865188
                                                                     //TestID: 1081650648124248124
