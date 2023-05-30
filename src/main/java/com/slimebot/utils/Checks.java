dfc72901fa977f09d748f46ffe990655c3200084
Matito <matitos@outlook.de>
2023-05-30 21:22:04 +0200

    Get StaffRole from Config

--- src/main/java/com/slimebot/utils/Checks.java
+++ src/main/java/com/slimebot/utils/Checks.java
@@ -18,9 +18,7 @@ import java.time.ZoneId;
 public class Checks {
 
     public static Boolean hasTeamRole(Member member, Guild guild){
-        Role staffRole = guild.getRoleById("1081650648124248124");  //ToDo get ID from a Config eg. Settings
-                                                                    //MainID: 1077266943003865188
-                                                                    //TestID: 1081650648124248124
+        Role staffRole = guild.getRoleById(Config.getProperty("config.yml", "staffRoleId"));
         return !(member.getRoles().contains(staffRole));
     }
 
