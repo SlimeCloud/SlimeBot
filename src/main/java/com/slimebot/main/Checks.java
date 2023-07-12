875bef85faca9b016f8b2bb71277800889c0dec7
MineKing <mineking9534@gmail.com>
2023-07-12 18:53:12 +0200

    Fix permission checks

--- src/main/java/com/slimebot/main/Checks.java
+++ src/main/java/com/slimebot/main/Checks.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public class Checks {
 	public static boolean hasTeamRole(Member member) {
 		return Optional.ofNullable(Main.database.getRole(member.getGuild(), DatabaseField.STAFF_ROLE))
-				.map(role -> member.getRoles().contains(role))
-				.orElse(member.hasPermission(Permission.MANAGE_SERVER));
+				.map(role -> !member.getRoles().contains(role))
+				.orElse(!member.hasPermission(Permission.MANAGE_SERVER));
 	}
 }
