3aea6981de71f4efa6f1ed3174da735f053f3e76
Matito <matitos@outlook.de>
2023-09-08 22:25:49 +0200

    get username instead of effectiveName

--- src/main/java/com/slimebot/commands/QuoteCommand.java
+++ src/main/java/com/slimebot/commands/QuoteCommand.java
@@ -55,7 +55,7 @@ public class QuoteCommand {
 										? "\n\n" + url
 										: ""
 								)
-								.setFooter("Zitiert von: " + event.getMember().getEffectiveName())
+								.setFooter("Zitiert von: " + event.getMember().getUser().getName())
 								.setTimestamp(timestamp != null ? timestamp : Instant.now())
 								.build()
 				).addActionRow(
