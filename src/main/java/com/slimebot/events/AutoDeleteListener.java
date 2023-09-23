ff5294438d48705dc20ca0387982fba1cd65f880
Matito <matitos@outlook.de>
2023-09-23 18:19:57 +0200

    user feedback on del
    Co-Authored-By: MineKing <69981672+MineKing9534@users.noreply.github.com>

--- src/main/java/com/slimebot/events/AutoDeleteListener.java
+++ src/main/java/com/slimebot/events/AutoDeleteListener.java
@@ -4,9 +4,12 @@ import com.slimebot.main.CommandPermission;
 import com.slimebot.main.Main;
 import com.slimebot.main.config.guild.AutoDeleteConfig;
 import com.slimebot.main.config.guild.GuildConfig;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -17,11 +20,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.EnumSet;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class AutoDeleteListener extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-		if (shouldDelete(event.getMessage(), event.getChannel())) event.getMessage().delete().queue();
+		if (shouldDelete(event.getMessage(), event.getChannel())) event.getMessage()
+				.delete()
+				.flatMap(x -> deleteFeedback(event.getAuthor(), event.getChannel().asGuildMessageChannel()))
+				.queue();
 	}
 
 	@Override
@@ -29,8 +36,8 @@ public class AutoDeleteListener extends ListenerAdapter {
 		if (!(event.getChannel() instanceof ThreadChannel thread)) return;
 
 		buildThreadDelete(thread)
-				.delay(5, TimeUnit.SECONDS)
-				.onErrorFlatMap(e -> buildThreadDelete(thread))
+				.onErrorFlatMap(e -> buildThreadDelete(thread).delay(5, TimeUnit.SECONDS))
+				.flatMap(x -> deleteFeedback(thread.getOwner().getUser(), thread.getParentChannel()))
 				.queueAfter(1, TimeUnit.SECONDS);
 	}
 
@@ -40,6 +47,31 @@ public class AutoDeleteListener extends ListenerAdapter {
 						? thread.delete()
 						: new CompletedRestAction<>(Main.jdaInstance, null)
 				);
+	}
+
+	private RestAction<?> deleteFeedback(User user, GuildChannel channel) {
+		return user.openPrivateChannel()
+				.flatMap(ch -> ch.sendMessageEmbeds(
+						new EmbedBuilder()
+								.setTitle("Nachricht gelöscht")
+								.setColor(GuildConfig.getColor(channel.getGuild()))
+								.setDescription("Deine Nachricht in " + channel.getAsMention() + " wurde gelöscht, da sie nicht den Filtern für den Kanal entspricht")
+								.addField(
+										"Folgende Nachrichten werden akzeptiert",
+										GuildConfig.getConfig(channel.getGuild()).getAutoDeleteConfig()
+												.map(a -> a.autoDeleteChannels.get(channel.getId()))
+												.map(f -> "```" +
+														f.stream()
+																.map(AutoDeleteConfig.Filter::getName)
+																.collect(Collectors.joining("\n")) +
+														"```"
+												)
+												.orElse("*Keine*"),
+										false
+								)
+								.build()
+				))
+				.mapToResult();
 	}
 
 	private boolean shouldDelete(Message message, Channel channel) {
