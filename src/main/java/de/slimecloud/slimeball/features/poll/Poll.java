96153722d2adf36890eaeb49f5469de2aee1070c
MineKing <mineking9534@gmail.com>
2023-12-28 10:09:43 +0100

    Improve PollCommand

--- src/main/java/de/slimecloud/slimeball/features/poll/Poll.java
+++ src/main/java/de/slimecloud/slimeball/features/poll/Poll.java
@@ -64,12 +64,14 @@ public class Poll implements DataClass<Poll> {
 		values.forEach((name, values) -> {
 			double percentage = count == 0 ? 0 : values.size() / (double) count;
 
-			result.append("\033[1m").append(StringUtil.padRight(name, maxLength.get())).append("\033[0m").append(": ")
+			result.append("\033[1m").append(StringUtil.padRight(name, maxLength.get())).append("\033[0m").append(maxLength.get() > 8 ? "\n" : "")
 					//Bar
 					.append("[").append(StringUtil.createProgressBar(percentage, 30)).append("]")
 					//Display percentage
 					.append(" \033[34;1m(").append((int) (percentage * 100)).append("%, ").append(values.size()).append(" Stimmen)\033[0m")
 					.append("\n");
+
+			if(maxLength.get() > 8) result.append("\n");
 		});
 
 		return "```ansi\n" + result + "```\n" + new HashSet<>(participants).size() + " Teilnehmer" + (max > 1 ? ", " + participants.size() + " Stimmen" : "");
--- src/main/java/de/slimecloud/slimeball/features/poll/PollCommand.java
+++ src/main/java/de/slimecloud/slimeball/features/poll/PollCommand.java
@@ -9,12 +9,15 @@ import de.slimecloud.slimeball.main.CommandPermission;
 import de.slimecloud.slimeball.main.SlimeBot;
 import de.slimecloud.slimeball.main.SlimeEmoji;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.requests.restaction.MessageEditAction;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -30,13 +33,15 @@ public class PollCommand {
 
 	@ApplicationCommandMethod
 	public void performCommand(@NotNull SlimeBot bot, @NotNull SlashCommandInteractionEvent event,
-	                           @Option(name = "question", description = "Die Frage") String question,
+	                           @Option( description = "Die Frage") String question,
 	                           @OptionArray(minCount = 2, maxCount = 5) @Option(name = "choice", description = "Die Auswahlmöglichkeiten") String[] options,
-	                           @IntegerDefault(1) @Option(description = "Die maximale Anzahl pro Nutzer", required = false, minValue = 1) int max
+	                           @IntegerDefault(1) @Option(description = "Die maximale Anzahl pro Nutzer", required = false, minValue = 1) int max,
+                               @Option(description = "Rolle, die erwähnt wird", required = false) Role role
 	) {
+		ReplyCallbackAction action = role == null ? event.deferReply() : event.reply(role.getAsMention());
+
 		//Send defer first to get the id of the message. Will be used as poll id
-		event.deferReply()
-				.flatMap(InteractionHook::retrieveOriginal)
+		action.flatMap(InteractionHook::retrieveOriginal)
 				.flatMap(mes -> {
 					//Create poll
 					Poll poll = bot.getPolls().createPoll(mes.getIdLong(), max, options);
@@ -60,7 +65,7 @@ public class PollCommand {
 							StringSelectMenu.create("poll:select")
 									.setPlaceholder("Wähle weise")
 									.addOptions(Arrays.stream(options)
-											.map(s -> SelectOption.of(s, String.valueOf(i.get()))
+											.map(s -> SelectOption.of(StringUtils.abbreviate(s, SelectOption.LABEL_MAX_LENGTH), String.valueOf(i.get()))
 													//Number keycap emoji
 													.withEmoji(SlimeEmoji.number(i.incrementAndGet()).getEmoji(event.getGuild()))
 											)
