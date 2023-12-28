b5e00a84060c35f237093ec2b4c9a967e4f91057
MineKing <mineking9534@gmail.com>
2023-12-28 12:04:10 +0100

    Remove test enum

--- src/main/java/de/slimecloud/slimeball/config/engine/ConfigFieldType.java
+++ src/main/java/de/slimecloud/slimeball/config/engine/ConfigFieldType.java
@@ -60,7 +60,6 @@ public enum ConfigFieldType {
 					.findAny().orElseThrow(),
 			Object::toString
 	),
-	ENUM_SET(null, null, null, null, (t, o) -> null, null),
 
 	STRING(OptionType.STRING, o -> o, OptionMapping::getAsString, x -> true, x -> x, Objects::toString),
 	INTEGER(OptionType.INTEGER, o -> o, OptionMapping::getAsInt, StringUtil::isInteger, Integer::parseInt, Objects::toString),
