e78a75640bacc08fe1eb215a2c42e9fa129cfabe
cyklon73 <cyklon698@gmail.com>
2024-01-01 18:31:21 +0100

    removed Statistic Base class

--- src/main/java/de/slimecloud/slimeball/features/statistic/MemberCount.java
+++ src/main/java/de/slimecloud/slimeball/features/statistic/MemberCount.java
@@ -1,18 +1,60 @@
 package de.slimecloud.slimeball.features.statistic;
 
 import de.slimecloud.slimeball.main.SlimeBot;
+import de.slimecloud.slimeball.util.types.AtomicString;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.events.guild.GenericGuildEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
+import java.util.function.Function;
 
-public class MemberCount extends StatisticChannel {
+
+public class MemberCount extends ListenerAdapter {
+
+	private final SlimeBot bot;
+	private final Function<StatisticConfig, Long> channel;
+	private final Function<StatisticConfig, String> format;
+
 	public MemberCount(SlimeBot bot) {
-		super(bot, StatisticConfig::getMemberCountChannel, StatisticConfig::getMemberCountFormat);
+		this.bot = bot;
+		this.channel = StatisticConfig::getMemberCountChannel;
+		this.format = StatisticConfig::getMemberCountFormat;
+	}
+
+	@Nullable
+	private StatisticConfig getConfig(long guild) {
+		return bot.loadGuild(guild).getStatistic().orElse(null);
+	}
+
+	@Nullable
+	private VoiceChannel getChannel(long guild) {
+		StatisticConfig config = getConfig(guild);
+		if (config==null) return null;
+		return bot.getJda().getVoiceChannelById(channel.apply(config));
+	}
+
+	@Nullable
+	private String getFormat(long guild) {
+		StatisticConfig config = getConfig(guild);
+		if (config==null) return null;
+		return format.apply(config);
+	}
+
+	@SuppressWarnings("ConstantConditions")
+	private void update(long guild, @NotNull Map<String, Object> values) {
+		VoiceChannel channel = getChannel(guild);
+		if (channel==null) return;
+		AtomicString format = new AtomicString(getFormat(guild));
+		if (format.isEmpty()) return;
+		values.forEach((k, v) -> format.set(format.get().replace("%" + k + "%", String.valueOf(v))));
+		channel.getManager().setName(format.get()).queue();
 	}
 
 	@Override
