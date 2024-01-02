2edfe637ec6465ade401f12df8df2a1d870deaf8
cyklon73 <cyklon698@gmail.com>
2024-01-02 23:29:21 +0100

    implemented BirthdayAlert

---
+++ src/main/java/de/slimecloud/slimeball/features/birthday/BirthdayAlert.java
@@ -0,0 +1,13 @@
+package de.slimecloud.slimeball.features.birthday;
+
+import de.slimecloud.slimeball.main.SlimeBot;
+import org.jetbrains.annotations.NotNull;
+
+public class BirthdayAlert {
+
+	private final SlimeBot bot;
+
+	public BirthdayAlert(@NotNull SlimeBot bot) {
+		this.bot = bot;
+	}
+}
