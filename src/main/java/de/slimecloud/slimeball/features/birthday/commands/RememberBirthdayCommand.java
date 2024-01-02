c9f96fc26876c386bd8c6ee2ee9b7036fd2a0be5
cyklon73 <cyklon698@gmail.com>
2024-01-02 15:19:58 +0100

    changed Month to enum and bug fix

--- src/main/java/de/slimecloud/slimeball/features/birthday/commands/RememberBirthdayCommand.java
+++ src/main/java/de/slimecloud/slimeball/features/birthday/commands/RememberBirthdayCommand.java
@@ -11,9 +11,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.utils.TimeFormat;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 @ApplicationCommand(name = "remember-birthday", description = "Speichere dein Geburtstag", scope = Scope.GUILD_GLOBAL)
@@ -21,19 +19,23 @@ public class RememberBirthdayCommand {
 
 	@ApplicationCommandMethod
 	@SuppressWarnings("ConstantConditions")
-	public void performCommand(@NotNull SlimeBot bot, @NotNull SlashCommandInteractionEvent event, @Option(name = "day", description = "der tag an dem du geburtstag hast", minValue = 1, maxValue = 31) Integer day, @Option(name = "month", description = "der monat in dem du geburtstag hast", minValue = 1, maxValue = 12) Integer month, @Option(name = "year", description = "das jahr in dem du geburtstag hast", minValue = 1900, maxValue = 2024, required = false) Integer year) {
+	public void performCommand(@NotNull SlimeBot bot, @NotNull SlashCommandInteractionEvent event, @Option(name = "day", description = "der tag an dem du geburtstag hast", minValue = 1, maxValue = 31) Integer day, @Option(name = "month", description = "der monat in dem du geburtstag hast") Month month, @Option(name = "year", description = "das jahr in dem du geburtstag hast", minValue = 1900, maxValue = 2024, required = false) Integer year) {
 		event.deferReply(true).queue();
-		ZonedDateTime dateTime = LocalDateTime.of(year==null ? 0 : year, month, day, 0, 0).atZone(ZoneId.systemDefault());
-		bot.getBirthdayTable().set(event.getMember(), dateTime.toInstant());
+		try {
+			ZonedDateTime dateTime = LocalDateTime.of(year==null ? 0 : year, month, day, 0, 0).atZone(ZoneId.systemDefault());
+			bot.getBirthdayTable().set(event.getMember(), dateTime.toInstant());
 
-		String date = year==null ? dateTime.format(DateTimeFormatter.ofPattern("d/M")) : TimeFormat.DATE_SHORT.format(dateTime);
+			String date = year==null ? dateTime.format(DateTimeFormatter.ofPattern("d/M")) : TimeFormat.DATE_SHORT.format(dateTime);
 
-		MessageEmbed embed = new EmbedBuilder()
-				.setColor(bot.getColor(event.getGuild()))
-				.setDescription(String.format("Dein geburtstag wurde auf den %s gesetzt", date))
-				.build();
+			MessageEmbed embed = new EmbedBuilder()
+					.setColor(bot.getColor(event.getGuild()))
+					.setDescription(String.format("Dein geburtstag wurde auf den %s gesetzt", date))
+					.build();
 
-		event.getHook().editOriginalEmbeds(embed).queue();
+			event.getHook().editOriginalEmbeds(embed).queue();
+		} catch (DateTimeException e) {
+			event.getHook().editOriginal(e.getMessage()).queue();
+		}
 	}
 
 }
