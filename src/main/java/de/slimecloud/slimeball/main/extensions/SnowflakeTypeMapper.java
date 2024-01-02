be6aca58c08d733c74ba71bdf725a97641e23845
MineKing <mineking9534@gmail.com>
2024-01-02 21:58:57 +0100

    Update JavaUtils

--- src/main/java/de/slimecloud/slimeball/main/extensions/SnowflakeTypeMapper.java
+++ src/main/java/de/slimecloud/slimeball/main/extensions/SnowflakeTypeMapper.java
@@ -2,6 +2,8 @@ package de.slimecloud.slimeball.main.extensions;
 
 import de.mineking.javautils.database.DatabaseManager;
 import de.mineking.javautils.database.TypeMapper;
+import de.mineking.javautils.database.type.DataType;
+import de.mineking.javautils.database.type.PostgresType;
 import de.slimecloud.slimeball.main.SlimeBot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
@@ -28,17 +30,17 @@ public class SnowflakeTypeMapper implements TypeMapper<Long, ISnowflake> {
 
 	@NotNull
 	@Override
-	public String getType(@NotNull DatabaseManager manager, @NotNull Class<?> type, @NotNull Field field) {
-		return "bigint";
+	public DataType getType(@NotNull DatabaseManager manager, @NotNull Class<?> type, @NotNull Field field) {
+		return PostgresType.BIG_INT;
 	}
 
 	@NotNull
 	@Override
-	public Argument createArgument(@NotNull DatabaseManager manager, @NotNull Class<?> type, @NotNull Field f, @Nullable ISnowflake value) {
+	public Argument createArgument(@NotNull DatabaseManager manager, @NotNull Class<?> type, @NotNull Field f, @Nullable Long value) {
 		return new Argument() {
 			@Override
 			public void apply(int position, PreparedStatement statement, StatementContext ctx) throws SQLException {
-				statement.setLong(position, value == null ? null : value.getIdLong());
+				statement.setLong(position, value);
 			}
 
 			@Override
@@ -48,10 +50,10 @@ public class SnowflakeTypeMapper implements TypeMapper<Long, ISnowflake> {
 		};
 	}
 
-	@NotNull
+	@Nullable
 	@Override
-	public String string(@NotNull DatabaseManager manager, @NotNull Class<?> type, @NotNull Field f, @Nullable ISnowflake value) {
-		return value == null ? "null" : value.getId();
+	public Long string(@NotNull DatabaseManager manager, @NotNull Class<?> type, @NotNull Field f, @Nullable ISnowflake value) {
+		return value == null ? null : value.getIdLong();
 	}
 
 	@Nullable
