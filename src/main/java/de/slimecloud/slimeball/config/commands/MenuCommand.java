ead0b60366473f9e9dc32323d0c3f06f29a1d96f
MineKing <mineking9534@gmail.com>
2024-01-02 22:52:25 +0100

    Add menu for EnumSet

--- src/main/java/de/slimecloud/slimeball/config/commands/MenuCommand.java
+++ src/main/java/de/slimecloud/slimeball/config/commands/MenuCommand.java
@@ -16,14 +16,18 @@ import de.mineking.discordutils.ui.state.DataState;
 import de.slimecloud.slimeball.config.GuildConfig;
 import de.slimecloud.slimeball.config.engine.CategoryInfo;
 import de.slimecloud.slimeball.config.engine.ConfigField;
+import de.slimecloud.slimeball.config.engine.ConfigFieldType;
+import de.slimecloud.slimeball.config.engine.KeyType;
 import de.slimecloud.slimeball.main.Main;
 import de.slimecloud.slimeball.main.SlimeBot;
 import de.slimecloud.slimeball.util.ErrorConsumer;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -107,7 +111,8 @@ public class MenuCommand {
 	private static MessageMenu createFieldMenu(@NotNull SlimeBot bot, @NotNull UIManager manager, @NotNull Function<GuildConfig, Object> instance, CategoryInfo category, @NotNull Field field) {
 		ConfigField info = field.getAnnotation(ConfigField.class);
 
-		if (List.class.isAssignableFrom(field.getType())) return createListValueMenu(bot, manager, instance, category, info, field);
+		if (EnumSet.class.isAssignableFrom(field.getType())) return createEnumSetValueMenu(bot, manager, instance, category, info, field);
+		if (Collection.class.isAssignableFrom(field.getType())) return createListValueMenu(bot, manager, instance, category, info, field);
 		if (Map.class.isAssignableFrom(field.getType())) return createMapValueMenu(bot, manager, instance, category, info, field);
 		return createValueMenu(bot, manager, instance, category, info, field);
 	}
@@ -174,7 +179,7 @@ public class MenuCommand {
 
 		components.add(new ButtonComponent("back", ButtonColor.GRAY, "Zurück").appendHandler(s -> manager.getMenu("config." + category.command()).display(s.event)));
 		components.add(new ButtonComponent("reset", ButtonColor.RED, "Zurücksetzten").appendHandler(s -> {
-			handle(bot, s, instance, o -> field.set(o, new ArrayList<>()));
+			handle(bot, s, instance, o -> field.set(o, field.getType().isAssignableFrom(ArrayList.class) ? new ArrayList<>() : new HashSet<>()));
 			s.update();
 		}));
 
@@ -213,17 +218,103 @@ public class MenuCommand {
 		);
 	}
 
+	@SuppressWarnings("unchecked")
+	private static <E extends Enum<E>> EnumSet<E> emptyEnumSet(@NotNull Class<?> type) {
+		return EnumSet.noneOf((Class<E>) type);
+	}
+
+	@NotNull
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	private static MessageMenu createEnumSetValueMenu(@NotNull SlimeBot bot, @NotNull UIManager manager, @NotNull Function<GuildConfig, Object> instance, @NotNull CategoryInfo category, @NotNull ConfigField info, @NotNull Field field) {
+		field.setAccessible(true);
+
+		Class<?> enumType = (Class<?>) ((ParameterizedType) field.getType().getGenericSuperclass()).getActualTypeArguments()[0];
+
+		List<Component<?>> components = new ArrayList<>();
+
+		components.add(new ButtonComponent("back", ButtonColor.GRAY, "Zurück").appendHandler(s -> manager.getMenu("config." + category.command()).display(s.event)));
+		components.add(new ButtonComponent("reset", ButtonColor.RED, "Zurücksetzten").appendHandler(s -> {
+			handle(bot, s, instance, o -> field.set(o, emptyEnumSet(enumType)));
+			s.update();
+		}));
+
+		components.add(new StringSelectComponent("value", s -> Arrays.stream(enumType.getEnumConstants())
+				.map(e -> SelectOption.of(e.toString(), ((Enum<?>) e).name())
+						.withDefault(s.<EnumSet<?>>getCache("value").contains(e))
+				)
+				.toList()
+		).setMinValues(0).setMaxValues(enumType.getEnumConstants().length).appendHandler((s, v) -> {
+			handle(bot, s, instance, o -> {
+				EnumSet value = (EnumSet) field.get(o);
+				value.clear();
+				value.addAll(v.stream().map(x -> Arrays.stream(enumType.getEnumConstants()).map(e -> (Enum<?>) e).filter(e -> e.name().equals(x.getValue())).findFirst().orElseThrow()).toList());
+			});
+			s.update();
+		}));
+
+		return manager.createMenu(
+				"config." + category.command() + "." + info.command(),
+				MessageRenderer.embed(s -> {
+					EnumSet<?> value = s.getCache("value");
+
+					return new EmbedBuilder()
+							.setDescription("## " + info.name() + "\n")
+							.setColor(bot.getColor(s.event.getGuild()))
+							.appendDescription(info.description())
+							.appendDescription("\n### Aktuelle Einträge\n")
+							.appendDescription(value.isEmpty() ? "*Keine Einträge*" : value.stream().map(e -> "- " + info.type().toString(e)).collect(Collectors.joining("\n")))
+							.build();
+				}),
+				ComponentRow.ofMany(components)
+		).cache(s -> s.setCache("value", getValue(bot, s, instance, field)));
+	}
+
 	@NotNull
 	private static MessageMenu createMapValueMenu(@NotNull SlimeBot bot, @NotNull UIManager manager, @NotNull Function<GuildConfig, Object> instance, @NotNull CategoryInfo category, @NotNull ConfigField info, @NotNull Field field) {
 		field.setAccessible(true);
 
+		ConfigFieldType keyType = field.getAnnotation(KeyType.class).value();
+
+		List<Component<?>> components = new ArrayList<>();
+
+		components.add(new ButtonComponent("back", ButtonColor.GRAY, "Zurück").appendHandler(s -> manager.getMenu("config." + category.command()).display(s.event)));
+		components.add(new ButtonComponent("reset", ButtonColor.RED, "Zurücksetzten").appendHandler(s -> {
+			handle(bot, s, instance, o -> field.set(o, field.getType().isAssignableFrom(HashMap.class) ? new HashMap<>() : new LinkedHashMap<>()));
+			s.update();
+		}));
+
+		Component<?> add = info.type().createComponent(manager, field.getType(), "config." + category.command() + "." + info.command(), "add", "Wert hinzufügen", (s, v) -> handle(bot, s, instance, o -> ((Collection) field.get(o)).add(v)));
+
+		Component<?> remove = new StringSelectComponent("remove", s -> MenuCommand.<Map<?, ?>>getValue(bot, s, instance, field).keySet().stream()
+				.map(e -> keyType.createSelectOption(bot, e))
+				.toList()
+		).setPlaceholder("Wert entfernen").appendHandler((s, v) -> {
+			handle(bot, s, instance, o -> ((Map<?, ?>) field.get(o)).remove(keyType.parse(field.getType(), v.get(0).getValue())));
+			s.update();
+		});
+
+		if (add instanceof EntitySelectComponent || add instanceof StringSelectComponent) {
+			components.add(0, add);
+			components.add(1, remove);
+		} else {
+			components.add(add);
+			components.add(remove);
+		}
+
 		return manager.createMenu(
 				"config." + category.command() + "." + info.command(),
-				MessageRenderer.embed(s -> new EmbedBuilder()
-						.setTitle(info.name())
-						.setColor(bot.getColor(s.event.getGuild()))
-						.build()
-				)
+				MessageRenderer.embed(s -> {
+					Map<?, ?> value = getValue(bot, s, instance, field);
+
+					return new EmbedBuilder()
+							.setDescription("## " + info.name() + "\n")
+							.setColor(bot.getColor(s.event.getGuild()))
+							.appendDescription(info.description())
+							.appendDescription("\n### Aktuelle Einträge\n")
+							.appendDescription(value.isEmpty() ? "*Keine Einträge*" : value.entrySet().stream().map(e -> "- " + keyType.toString(e.getKey()) + " = " + info.type().toString(e.getValue())).collect(Collectors.joining("\n")))
+							.build();
+				}),
+				ComponentRow.ofMany(components)
 		);
 	}
 
