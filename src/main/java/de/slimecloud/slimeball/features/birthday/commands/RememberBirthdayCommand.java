85599cd125772758376f2a55e8a1cda205e19e19
cyklon73 <cyklon698@gmail.com>
2024-01-02 14:16:49 +0100

    implemented RememberBirthdayCommand

---
+++ src/main/java/de/slimecloud/slimeball/features/birthday/commands/RememberBirthdayCommand.java
@@ -0,0 +1,37 @@
+package de.slimecloud.slimeball.features.birthday.commands;
+
+import de.mineking.discordutils.commands.ApplicationCommand;
+import de.mineking.discordutils.commands.ApplicationCommandMethod;
+import de.mineking.discordutils.commands.condition.Scope;
+import de.mineking.discordutils.commands.option.Option;
+import de.slimecloud.slimeball.main.SlimeBot;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.utils.TimeFormat;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ApplicationCommand(name = "remember-birthday", description = "Speichere dein Geburtstag", scope = Scope.GUILD_GLOBAL)
+public class RememberBirthdayCommand {
+
+	@ApplicationCommandMethod
+	@SuppressWarnings("ConstantConditions")
+	public void performCommand(@NotNull SlimeBot bot, @NotNull SlashCommandInteractionEvent event, @Option(name = "day", description = "der tag an dem du geburtstag hast", minValue = 1, maxValue = 31) Integer day, @Option(name = "month", description = "der monat in dem du geburtstag hast", minValue = 1, maxValue = 12) Integer month, @Option(name = "year", description = "das jahr in dem du geburtstag hast", minValue = 1900, maxValue = 2024, required = false) Integer year) {
+		event.deferReply(true).queue();
+		LocalDateTime dateTime = LocalDateTime.of(year==null ? 0 : year, month, day, 0, 0);
+		bot.getBirthdayTable().set(event.getMember(), dateTime);
+
+		String date = year==null ? dateTime.format(DateTimeFormatter.ofPattern("d/M")) : TimeFormat.DATE_SHORT.format(dateTime);
+
+		MessageEmbed embed = new EmbedBuilder()
+				.setColor(bot.getColor(event.getGuild()))
+				.setDescription(String.format("Dein geburtstag wurde auf den %s gesetzt", date))
+				.build();
+
+		event.getHook().editOriginalEmbeds(embed).queue();
+	}
+
+}
