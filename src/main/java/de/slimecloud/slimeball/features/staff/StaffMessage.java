82e3797af36c33bb81dfabb8641f573ee363b2ec
MineKing <mineking9534@gmail.com>
2024-01-03 09:42:09 +0100

    remove staff configuration when message is deleted

--- src/main/java/de/slimecloud/slimeball/features/staff/StaffMessage.java
+++ src/main/java/de/slimecloud/slimeball/features/staff/StaffMessage.java
@@ -1,17 +1,34 @@
 package de.slimecloud.slimeball.features.staff;
 
+import de.slimecloud.slimeball.config.GuildConfig;
 import de.slimecloud.slimeball.main.SlimeBot;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleRemoveEvent;
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
 public class StaffMessage extends ListenerAdapter {
 	private final SlimeBot bot;
+
+	@Override
+	public void onMessageDelete(@NotNull MessageDeleteEvent event) {
+		if(!event.isFromGuild()) return;
+
+		GuildConfig guildConfig = bot.loadGuild(event.getGuild());
+		guildConfig.getStaff().ifPresent(config -> {
+			if (config.getMessage() == event.getMessageIdLong()) {
+				config.disable(event.getGuild());
+				guildConfig.setStaff(null);
+
+				guildConfig.save();
+			}
+		});
+	}
 
 	@Override
 	public void onGuildMemberRoleAdd(@NotNull GuildMemberRoleAddEvent event) {
