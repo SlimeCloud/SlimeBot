ba4e63b2a6ce968c173b866b3af32688f1ca5bb6
cyklon73 <cyklon698@gmail.com>
2024-01-06 23:26:24 +0100

    implemented methods to check if a time is the same day

---
+++ src/main/java/de/slimecloud/slimeball/util/TimeUtil.java
@@ -0,0 +1,26 @@
+package de.slimecloud.slimeball.util;
+
+import de.slimecloud.slimeball.main.SlimeBot;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
+public class TimeUtil {
+
+	public static boolean isSameDay(Instant instant, Instant other) {
+		return isSameDay(instant, other, false);
+	}
+
+	public static boolean isSameDay(ZonedDateTime zdt, ZonedDateTime other) {
+		return isSameDay(zdt, other, false);
+	}
+
+	public static boolean isSameDay(Instant instant, Instant other, boolean ignoreYear) {
+		return isSameDay(ZonedDateTime.ofInstant(instant, SlimeBot.timezone), ZonedDateTime.ofInstant(other, SlimeBot.timezone), ignoreYear);
+	}
+
+	public static boolean isSameDay(ZonedDateTime zdt, ZonedDateTime other, boolean ignoreYear) {
+		return (ignoreYear || zdt.getYear()==other.getYear()) && zdt.getMonth().equals(other.getMonth()) && zdt.getDayOfMonth()==other.getDayOfMonth();
+	}
+
+}
