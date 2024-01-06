c8bd798b141eaaacbd6b88722c2193f52581fdf9
MineKing <mineking9534@gmail.com>
2024-01-06 11:45:30 +0100

    Add role ping on error

--- src/main/java/de/slimecloud/slimeball/config/LogForwarding.java
+++ src/main/java/de/slimecloud/slimeball/config/LogForwarding.java
@@ -2,38 +2,41 @@ package de.slimecloud.slimeball.config;
 
 import de.mineking.discordutils.console.RedirectTarget;
 import de.slimecloud.slimeball.config.engine.Required;
+import de.slimecloud.slimeball.main.SlimeBot;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 public record LogForwarding(@Required Type type, @Required String id) {
 	enum Type {
 		USER {
 			@NotNull
 			@Override
-			public RedirectTarget build(@NotNull String id) {
+			public RedirectTarget<SlimeBot> build(@NotNull String id) {
 				return RedirectTarget.directMessage(Long.parseLong(id));
 			}
 		},
 		CHANNEL {
 			@NotNull
 			@Override
-			public RedirectTarget build(@NotNull String id) {
+			public RedirectTarget<SlimeBot> build(@NotNull String id) {
 				return RedirectTarget.channel(Long.parseLong(id));
 			}
 		},
 		WEBHOOK {
 			@NotNull
 			@Override
-			public RedirectTarget build(@NotNull String value) {
+			public RedirectTarget<SlimeBot> build(@NotNull String value) {
 				return RedirectTarget.webhook(value);
 			}
 		};
 
 		@NotNull
-		public abstract RedirectTarget build(@NotNull String value);
+		public abstract RedirectTarget<SlimeBot> build(@NotNull String value);
 	}
 
 	@NotNull
-	public RedirectTarget build() {
-		return type.build(id);
+	public RedirectTarget<SlimeBot> build() {
+		return RedirectTarget.pingRoleOnError(type.build(id), bot -> Optional.ofNullable(bot.getConfig().getDeveloperRole()).map(bot.getJda()::getRoleById));
 	}
 }
