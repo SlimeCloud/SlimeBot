3a443f6d52b1fd692590331086f097a83a7d863b
Matito <matitos@outlook.de>
2024-01-08 20:01:45 +0100

    add usable placeholders to description

--- src/main/java/de/slimecloud/slimeball/features/statistic/StatisticConfig.java
+++ src/main/java/de/slimecloud/slimeball/features/statistic/StatisticConfig.java
@@ -18,18 +18,18 @@ public class StatisticConfig extends ConfigCategory {
 	@ConfigField(name = "MemberCount Kanal", command = "member_channel", description = "Kanal, der die aktuelle Anzahl an Server-Mitgliedern anzeigt", type = ConfigFieldType.VOICE_CHANNEL)
 	private Long memberCountChannel;
 
-	@ConfigField(name = "MemberCount Formatierung", command = "member_format", description = "Format für die Mitgliederanzahl-Anzeige", type = ConfigFieldType.STRING)
+	@ConfigField(name = "MemberCount Formatierung", command = "member_format", description = "Format für die Mitgliederanzahl-Anzeige. placeholder: %members%", type = ConfigFieldType.STRING)
 	private String memberCountFormat = "Total Members: %members%";
 
 
-	@ConfigField(name = "RoleMemberCount Default Formatierung", command = "default_format", description = "Standard Format für Rollen-Mitgliederanzahl-Anzeigen", type = ConfigFieldType.STRING)
+	@ConfigField(name = "RoleMemberCount Default Formatierung", command = "default_format", description = "Standard Format für Rollen-Mitgliederanzahl-Anzeigen. placeholders: %role_name%, %members%", type = ConfigFieldType.STRING)
 	private String defaultRoleFormat = "%role_name%: %members%";
 
 	@ConfigField(name = "RoleMemberCount Kanal", command = "role_channel", description = "Kanäle, in denen Die Mitgliederanzahl mit einer bestimmten Rolle angezeigt werden", type = ConfigFieldType.ROLE)
 	@Info(keyType = ConfigFieldType.VOICE_CHANNEL)
 	private Map<Long, Long> roleMemberCountChannel = new HashMap<>();
 
-	@ConfigField(name = "RoleMemberCount Formatierung", command = "role_format", description = "Format für die Rollen-Mitgliederanzahl-Anzeigen", type = ConfigFieldType.STRING)
+	@ConfigField(name = "RoleMemberCount Formatierung", command = "role_format", description = "Format für die Rollen-Mitgliederanzahl-Anzeigen. placeholders: %role_name%, %members%", type = ConfigFieldType.STRING)
 	@Info(keyType = ConfigFieldType.VOICE_CHANNEL)
 	private Map<Long, String> roleMemberCountFormat = new HashMap<>();
 
