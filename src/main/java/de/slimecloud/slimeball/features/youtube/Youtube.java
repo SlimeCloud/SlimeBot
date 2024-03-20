49e743dfe9dea9db345336544e46b94f93876522
cyklon73 <cyklon698@gmail.com>
2024-03-20 15:00:06 +0100

    implemented Youtube API

---
+++ src/main/java/de/slimecloud/slimeball/features/youtube/Youtube.java
@@ -0,0 +1,45 @@
+package de.slimecloud.slimeball.features.youtube;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import de.slimecloud.slimeball.main.SlimeBot;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Youtube {
+
+	private final String API_KEY;
+	private final List<GuildYoutube> guildInstances = new ArrayList<>();
+
+	public Youtube(String apiKey, SlimeBot bot) {
+		this.API_KEY = apiKey;
+		bot.getJda().getGuilds().forEach(g -> bot.loadGuild(g).getYoutube().ifPresent(c -> guildInstances.add(new GuildYoutube(this, c))));
+		bot.getExecutor().scheduleAtFixedRate(() -> guildInstances.forEach(GuildYoutube::schedule), 2, 2, TimeUnit.SECONDS);
+	}
+
+	public String getLastVideo(String id) {
+		try {
+			URL requestURL = new URI(String.format("https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=snippet,id&order=date&maxResults=20", API_KEY, id)).toURL();
+			HttpURLConnection urlConnection = (HttpURLConnection) requestURL.openConnection();
+			urlConnection.setRequestMethod("GET");
+
+			JsonObject response = JsonParser.parseReader(new InputStreamReader(urlConnection.getInputStream())).getAsJsonObject();
+			JsonArray videos = response.getAsJsonArray("items");
+
+			if(videos.size() <= 0) return "";
+			return videos.get(0).getAsJsonObject().getAsJsonObject("id").get("videoId").getAsString();
+		} catch (URISyntaxException | IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
