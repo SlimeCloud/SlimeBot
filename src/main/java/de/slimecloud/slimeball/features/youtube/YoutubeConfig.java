b42dec811b4028909889548a77875e6702a5151c
cyklon73 <cyklon698@gmail.com>
2024-03-23 13:59:56 +0100

    implemented YoutubeConfig

---
+++ src/main/java/de/slimecloud/slimeball/features/youtube/YoutubeConfig.java
@@ -0,0 +1,15 @@
+package de.slimecloud.slimeball.features.youtube;
+
+import de.slimecloud.slimeball.config.engine.Required;
+import lombok.Getter;
+
+@Getter
+public class YoutubeConfig {
+
+	@Required
+	private String youtubeChannelId;
+
+	@Required
+	private int updateRate; // seconds
+
+}
