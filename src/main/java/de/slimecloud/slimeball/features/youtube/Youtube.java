4c380a4c5aa0062687211e4f41750e1ca733d539
cyklon73 <cyklon698@gmail.com>
2024-03-23 15:15:41 +0100

    implemented video check

--- src/main/java/de/slimecloud/slimeball/features/youtube/Youtube.java
+++ src/main/java/de/slimecloud/slimeball/features/youtube/Youtube.java
@@ -3,8 +3,12 @@ package de.slimecloud.slimeball.features.youtube;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import de.slimecloud.slimeball.features.youtube.model.SearchResult;
+import de.slimecloud.slimeball.features.youtube.model.Video;
+import de.slimecloud.slimeball.main.Main;
 import de.slimecloud.slimeball.main.SlimeBot;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,8 +16,6 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
@@ -21,25 +23,38 @@ public class Youtube {
 
 	private final String API_KEY;
 	private final SlimeBot bot;
-	private final List<GuildYoutube> guildInstances = new ArrayList<>();
+
+	private Video lastVideo;
 
 
 	public void init() {
-		bot.getJda().getGuilds().forEach(g -> bot.loadGuild(g).getYoutube().ifPresent(c -> guildInstances.add(new GuildYoutube(this, c))));
-		bot.getExecutor().scheduleAtFixedRate(() -> guildInstances.forEach(GuildYoutube::schedule), 2, 2, TimeUnit.SECONDS);
+		int delay = bot.getConfig().getYoutube().get().getUpdateRate();
+		lastVideo = getLastVideo();
+		bot.getExecutor().scheduleAtFixedRate(this::schedule, delay, delay, TimeUnit.SECONDS);
 	}
 
-	public String getLastVideo(String id) {
+	private void schedule() {
+		Video lastCheckedVideo = getLastVideo();
+		if (lastCheckedVideo != null && !lastCheckedVideo.id().equals(lastVideo.id())) {
+			new YoutubeVideoEvent(lastCheckedVideo).callEvent();
+			this.lastVideo = lastCheckedVideo;
+		}
+	}
+
+	//returns null if no videos found on the channel
+	@Nullable
+	public Video getLastVideo() {
 		try {
-			URL requestURL = new URI(String.format("https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=snippet,id&order=date&maxResults=20", API_KEY, id)).toURL();
+			URL requestURL = new URI(String.format("https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=snippet,id&order=date&maxResults=1", API_KEY, bot.getConfig().getYoutube().get().getYoutubeChannelId())).toURL();
 			HttpURLConnection urlConnection = (HttpURLConnection) requestURL.openConnection();
 			urlConnection.setRequestMethod("GET");
 
 			JsonObject response = JsonParser.parseReader(new InputStreamReader(urlConnection.getInputStream())).getAsJsonObject();
 			JsonArray videos = response.getAsJsonArray("items");
 
-			if(videos.size() <= 0) return "";
-			return videos.get(0).getAsJsonObject().getAsJsonObject("id").get("videoId").getAsString();
+			if(videos.size() <= 0) return null;
+
+			return Video.ofSearch(Main.json.fromJson(videos.get(0), SearchResult.class));
 		} catch (URISyntaxException | IOException e) {
 			throw new RuntimeException(e);
 		}
