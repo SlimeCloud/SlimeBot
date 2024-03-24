3b74831b15a0c242f51aea22d660a87e6d044e43
cyklon73 <cyklon698@gmail.com>
2024-03-24 14:24:55 +0100

    added @NotNull

--- src/main/java/de/slimecloud/slimeball/features/alerts/youtube/model/Channel.java
+++ src/main/java/de/slimecloud/slimeball/features/alerts/youtube/model/Channel.java
@@ -1,7 +1,10 @@
 package de.slimecloud.slimeball.features.alerts.youtube.model;
 
+import org.jetbrains.annotations.NotNull;
+
 public record Channel(String id, ChannelSnippet snippet) {
 
+	@NotNull
 	public String getTitle() {
 		return snippet.title();
 	}
