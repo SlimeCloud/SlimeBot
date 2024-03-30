fd99950097cdd01e24341cb212c7edc6a5960617
cyklon73 <cyklon698@gmail.com>
2024-03-30 12:23:51 +0100

    replaced Constructor with @RequiredArgsConstructor

--- src/main/java/de/slimecloud/slimeball/features/alerts/youtube/YoutubeVideoEvent.java
+++ src/main/java/de/slimecloud/slimeball/features/alerts/youtube/YoutubeVideoEvent.java
@@ -3,15 +3,13 @@ package de.slimecloud.slimeball.features.alerts.youtube;
 import de.cyklon.jevent.Event;
 import de.slimecloud.slimeball.features.alerts.youtube.model.Video;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class YoutubeVideoEvent extends Event {
 
 	private final Video video;
-
-	public YoutubeVideoEvent(Video video) {
-		this.video = video;
-	}
 
 	public boolean isLive() {
 		return video.isLive();
