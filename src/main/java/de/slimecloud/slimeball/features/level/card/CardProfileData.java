4814e4288c249db4ee19dca5fc01548954b06f98
Matito <matitos@outlook.de>
2024-05-15 21:42:48 +0200

    fix card database
    Co-Authored-By: MineKing <69981672+MineKing9534@users.noreply.github.com>

--- src/main/java/de/slimecloud/slimeball/features/level/card/CardProfileData.java
+++ src/main/java/de/slimecloud/slimeball/features/level/card/CardProfileData.java
@@ -43,64 +43,64 @@ public class CardProfileData implements DataClass<CardProfileData>, ListEntry {
 
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color progressbarColor = new Color(105, 227, 73, 240);
+	private Color progressbarColor = new Color(105, 227, 73, 240);
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color progressbarBGColor = new Color(150, 150, 150, 50);
+	private Color progressbarBGColor = new Color(150, 150, 150, 50);
 	@Column
 	@Info(keyType = ConfigFieldType.ENUM)
-	private final Style progressbarStyle = Style.ROUND_SQUARE;
+	private Style progressbarStyle = Style.ROUND_SQUARE;
 
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color progressbarBorderColor = new Color(68, 140, 41, 255);
+	private Color progressbarBorderColor = new Color(68, 140, 41, 255);
 	@Column
 	@Info(keyType = ConfigFieldType.INTEGER)
-	private final int progressbarBorderWidth = 5;
-
-	@Column
-	@Info(keyType = ConfigFieldType.ENUM)
-	private final Style avatarStyle = Style.ROUND_SQUARE;
-	@Column
-	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color avatarBorderColor = TRANSPARENT;
-	@Column
-	@Info(keyType = ConfigFieldType.INTEGER)
-	private final int avatarBorderWidth = 10;
+	private int progressbarBorderWidth = 5;
 
 	@Column
 	@Info(keyType = ConfigFieldType.ENUM)
-	private final Style badgeStyle = Style.ROUND_SQUARE;
+	private Style avatarStyle = Style.ROUND_SQUARE;
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color badgeBorderColor = new Color(68, 140, 41, 255);
+	private Color avatarBorderColor = TRANSPARENT;
 	@Column
 	@Info(keyType = ConfigFieldType.INTEGER)
-	private final int badgeBorderWidth = 0;
+	private int avatarBorderWidth = 10;
+
+	@Column
+	@Info(keyType = ConfigFieldType.ENUM)
+	private Style badgeStyle = Style.ROUND_SQUARE;
+	@Column
+	@Info(keyType = ConfigFieldType.COLOR)
+	private Color badgeBorderColor = new Color(68, 140, 41, 255);
+	@Column
+	@Info(keyType = ConfigFieldType.INTEGER)
+	private int badgeBorderWidth = 0;
 
 
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color backgroundColor = new Color(30, 30, 30, 200);
+	private Color backgroundColor = new Color(30, 30, 30, 200);
 	@Column
 	@Info(keyType = ConfigFieldType.URL)
-	private final String backgroundImageURL = "";
+	private String backgroundImageURL;
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color backgroundBorderColor = new Color(68, 140, 41, 255);
+	private Color backgroundBorderColor = new Color(68, 140, 41, 255);
 	@Column
 	@Info(keyType = ConfigFieldType.INTEGER)
-	private final int backgroundBorderWidth = 10;
+	private int backgroundBorderWidth = 10;
 
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color fontColor = Color.WHITE;
+	private Color fontColor = Color.WHITE;
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color fontSecondaryColor = Color.GRAY;
+	private Color fontSecondaryColor = Color.GRAY;
 	@Column
 	@Info(keyType = ConfigFieldType.COLOR)
-	private final Color fontLevelColor = new Color(97, 180, 237);
+	private Color fontLevelColor = new Color(97, 180, 237);
 
 	@Column
 	@Info(keyType = ConfigFieldType.STRING)
