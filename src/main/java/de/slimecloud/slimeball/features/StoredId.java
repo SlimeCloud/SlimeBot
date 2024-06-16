cf90635b04d79b2d4045ede4a84c79ab784065f9
MineKing <mineking9534@gmail.com>
2024-06-16 20:00:14 +0200

    Register StoredId columns

--- src/main/java/de/slimecloud/slimeball/features/StoredId.java
+++ src/main/java/de/slimecloud/slimeball/features/StoredId.java
@@ -1,11 +1,12 @@
 package de.slimecloud.slimeball.features;
 
+import de.mineking.databaseutils.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class StoredId {
-	private String type;
-	private String id;
+	@Column(key = true) private String type;
+	@Column(key = true) private String id;
 }
