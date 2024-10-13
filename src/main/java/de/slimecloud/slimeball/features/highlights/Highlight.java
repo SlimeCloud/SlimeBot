a166c58f3095eb28c2e770632d06982be465306b
cyklon73 <cyklon698@gmail.com>
2024-10-13 13:53:20 +0200

    implemented Highlight

---
+++ src/main/java/de/slimecloud/slimeball/features/highlights/Highlight.java
@@ -0,0 +1,41 @@
+package de.slimecloud.slimeball.features.highlights;
+
+import de.mineking.databaseutils.Column;
+import de.mineking.databaseutils.DataClass;
+import de.mineking.databaseutils.Table;
+import de.slimecloud.slimeball.main.SlimeBot;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.UserSnowflake;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+public class Highlight implements DataClass<Highlight> {
+
+	private final SlimeBot bot;
+
+	@Column(key = true)
+	private final Guild guild;
+
+	@Column(key = true)
+	private final String phrase;
+
+	@Column
+	private final Set<UserSnowflake> users;
+
+	public Highlight(SlimeBot bot) {
+		this(bot, null, null, null);
+	}
+
+	@NotNull
+	@Override
+	public Table<Highlight> getTable() {
+		return bot.getHighlights();
+	}
+
+
+}
