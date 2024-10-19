5e82e4227baa88d399a52c1f51d2944776ad1b7b
cyklon73 <cyklon698@gmail.com>
2024-10-19 23:48:10 +0200

    register HighlightListener

--- src/main/java/de/slimecloud/slimeball/features/highlights/commands/HighlightCommand.java
+++ src/main/java/de/slimecloud/slimeball/features/highlights/commands/HighlightCommand.java
@@ -1,18 +1,21 @@
 package de.slimecloud.slimeball.features.highlights.commands;
 
+import de.mineking.discordutils.DiscordUtils;
 import de.mineking.discordutils.commands.ApplicationCommand;
 import de.mineking.discordutils.commands.Command;
 import de.mineking.discordutils.commands.Setup;
 import de.mineking.discordutils.commands.condition.Scope;
 import de.mineking.discordutils.commands.context.ICommandContext;
-import de.mineking.discordutils.list.ListManager;
+import de.slimecloud.slimeball.features.highlights.HighlightListener;
 import de.slimecloud.slimeball.main.SlimeBot;
 import org.jetbrains.annotations.NotNull;
 
 @ApplicationCommand(name = "highlights", description = "Verwaltet Highlights", scope = Scope.GUILD)
 public class HighlightCommand {
 	@Setup
-	public static void setup(@NotNull SlimeBot bot, @NotNull Command<ICommandContext> command, @NotNull ListManager<ICommandContext> manager) {
+	public static void setup(@NotNull SlimeBot bot, @NotNull DiscordUtils<?> discordUtils, @NotNull Command<ICommandContext> command) {
+		discordUtils.getJDA().addEventListener(new HighlightListener(bot));
+
 		command.addSubcommand(HighlightListCommand.class);
 		command.addSubcommand(HighlightAddCommand.class);
 		command.addSubcommand(HighlightDeleteCommand.class);
