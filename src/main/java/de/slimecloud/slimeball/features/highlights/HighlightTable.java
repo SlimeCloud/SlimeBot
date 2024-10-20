9b90d5c15371672409d759de228416336374379f
cyklon73 <cyklon698@gmail.com>
2024-10-20 18:33:03 +0200

    removed build() and save() method

--- src/main/java/de/slimecloud/slimeball/features/highlights/HighlightTable.java
+++ src/main/java/de/slimecloud/slimeball/features/highlights/HighlightTable.java
@@ -18,22 +18,12 @@ import java.util.Set;
 public interface HighlightTable extends Table<Highlight> {
 
 	@NotNull
-	default Highlight build(@NotNull Guild guild, @NotNull String phrase, @NotNull Set<UserSnowflake> users) {
-		return new Highlight(getManager().getData("bot"), guild, phrase, users);
-	}
-
-	@NotNull
-	default Highlight save(@NotNull Guild guild, @NotNull String phrase, @NotNull Set<UserSnowflake> users) {
-		return upsert(build(guild, phrase, users));
-	}
-
-	@NotNull
 	default Highlight set(@NotNull Member member, @NotNull String phrase) {
 		Set<UserSnowflake> users = getUsers(member.getGuild(), phrase);
-		Highlight highlight = build(member.getGuild(), phrase, users);
+		Highlight highlight = new Highlight(getManager().getData("bot"), member.getGuild(), phrase, users);
 		if (!users.contains(member) && !new HighlightSetEvent(highlight, member).callEvent()) {
 			users.add(member);
-			return save(member.getGuild(), phrase, users);
+			return upsert(new Highlight(getManager().getData("bot"), member.getGuild(), phrase, users));
 		}
 		return highlight;
 	}
@@ -45,10 +35,10 @@ public interface HighlightTable extends Table<Highlight> {
 	default Highlight remove(@NotNull Member member, @NotNull String phrase) {
 		Set<UserSnowflake> users = getUsers(member.getGuild(), phrase);
 		if (!users.contains(member.getUser())) return null;
-		Highlight highlight = build(member.getGuild(), phrase, users);
+		Highlight highlight = new Highlight(getManager().getData("bot"), member.getGuild(), phrase, users);
 		if (users.contains(member.getUser()) && !new HighlightRemoveEvent(highlight, member).callEvent()) {
 			users.remove(member.getUser());
-			return save(member.getGuild(), phrase, users);
+			return upsert(new Highlight(getManager().getData("bot"), member.getGuild(), phrase, users));
 		}
 		return highlight;
 	}
