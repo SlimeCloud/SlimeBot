32b716c2befe97d6b8dfffb9d0863a76308475f6
MineKing <mineking9534@gmail.com>
2024-11-09 12:48:59 +0100

    Add message update command

--- src/main/java/de/slimecloud/slimeball/features/message/MessageCommand.java
+++ src/main/java/de/slimecloud/slimeball/features/message/MessageCommand.java
@@ -10,6 +10,7 @@ import de.mineking.discordutils.commands.option.Option;
 import de.slimecloud.slimeball.config.GuildConfig;
 import de.slimecloud.slimeball.main.CommandPermission;
 import de.slimecloud.slimeball.main.SlimeBot;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
@@ -21,33 +22,47 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
-@ApplicationCommand(name = "message", description = "Sendet eine gespeicherte Nachricht als Bot", scope = Scope.GUILD)
+@ApplicationCommand(name = "message", description = "Verwaltet nachrichten, die vom Bot gesendet werden", scope = Scope.GUILD)
 public class MessageCommand {
 	public final CommandPermission permission = CommandPermission.TEAM;
 	public final IRegistrationCondition<ICommandContext> condition = (manager, guild, cache) -> cache.<GuildConfig>getState("config").getAutoMessage().isPresent();
 
 	public static void startScheduler(@NotNull SlimeBot bot) {
-		bot.getExecutor().scheduleAtFixedRate(() -> bot.getJda().getGuilds().forEach(guild -> {
-			GuildConfig config = bot.loadGuild(guild);
-			config.getAutoMessage().ifPresent(am -> am.update(guild).queue(x -> config.save()));
-		}), 0, 1, TimeUnit.HOURS);
+		bot.getExecutor().scheduleAtFixedRate(() -> bot.getJda().getGuilds().forEach(guild -> updateMessage(bot, guild)), 0, 1, TimeUnit.HOURS);
 	}
 
-	@Autocomplete("message")
-	public void autocomplete(@NotNull SlimeBot bot, @NotNull CommandAutoCompleteInteractionEvent event) {
-		event.replyChoices(bot.loadGuildResource(event.getGuild(), "messages", false, File::list)
-				.map(list -> Arrays.stream(list).map(e -> new Command.Choice(e, e)).toList())
-				.orElse(Collections.emptyList())
-		).queue();
+	public static void updateMessage(@NotNull SlimeBot bot, @NotNull Guild guild) {
+		GuildConfig config = bot.loadGuild(guild);
+		config.getAutoMessage().ifPresent(am -> am.update(guild).queue(x -> config.save()));
 	}
 
-	@ApplicationCommandMethod
-	public void performCommand(@NotNull SlimeBot bot, @NotNull SlashCommandInteractionEvent event,
-                               @Option(description = "Der Name der Datei") String message
-	) {
-		GuildConfig config = bot.loadGuild(event.getGuild());
-		config.getAutoMessage().ifPresent(am -> am.sendMessage(event.getChannel().asGuildMessageChannel(), message).queue(x -> config.save()));
+	@ApplicationCommand(name = "send", description = "Sendet eine gespeicherte Nachricht als Bot")
+	public class SendCommand {
+		@Autocomplete("message")
+		public void autocomplete(@NotNull SlimeBot bot, @NotNull CommandAutoCompleteInteractionEvent event) {
+			event.replyChoices(bot.loadGuildResource(event.getGuild(), "messages", false, File::list)
+					.map(list -> Arrays.stream(list).map(e -> new Command.Choice(e, e)).toList())
+					.orElse(Collections.emptyList())
+			).queue();
+		}
 
-		event.deferReply(true).flatMap(InteractionHook::deleteOriginal).queue();
+		@ApplicationCommandMethod
+		public void performCommand(@NotNull SlimeBot bot, @NotNull SlashCommandInteractionEvent event,
+	                               @Option(description = "Der Name der Datei") String message
+		) {
+			GuildConfig config = bot.loadGuild(event.getGuild());
+			config.getAutoMessage().ifPresent(am -> am.sendMessage(event.getChannel().asGuildMessageChannel(), message).queue(x -> config.save()));
+
+			event.deferReply(true).flatMap(InteractionHook::deleteOriginal).queue();
+		}
+	}
+
+	@ApplicationCommand(name = "update", description = "Updated alle Nachrichten")
+	public class UpdateCommand {
+		@ApplicationCommandMethod
+		public void performCommand(@NotNull SlimeBot bot, @NotNull SlashCommandInteractionEvent event) {
+			updateMessage(bot, event.getGuild());
+			event.deferReply(true).flatMap(InteractionHook::deleteOriginal).queue();
+		}
 	}
 }
